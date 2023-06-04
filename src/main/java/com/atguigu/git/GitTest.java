f83d8780883e9ff8e889527d277cae05176c2f33
atguiguyuebuq <135344488+atguiguyuebuq@users.noreply.github.com>
2023-06-04 14:24:41 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,8 +12,9 @@ public class GitTest {
         System.out.println("hello git3~");
         System.out.println("hello git4~");
         System.out.println("master test");
-        System.out.println("hot-fix text");
-        System.out.println("push text");
+        System.out.println("hot-fix test");
+        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
                                                    
