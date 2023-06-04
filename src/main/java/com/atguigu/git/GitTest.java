2efffe345bf80b8c9cb0fb7c150e97d8c4d9b313
Ling <Lingo@git.com>
2023-06-04 08:48:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author Ling
+ * @create 2023-06-04 08:40
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+    }
+}
+                                                   
